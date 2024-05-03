6d1e4836d2e75a3cab8544a836e55d482afa6231
Christian Rockenbach <microsoft@chrock.de>
2024-05-03 17:46:40 +0200

    kk-sqash-1

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,12 +3,13 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import {MatDividerModule} from '@angular/material/divider';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink,MatToolbarModule,MatIconModule,MatButtonModule],
+  imports: [RouterLink,MatToolbarModule,MatIconModule,MatButtonModule,MatDividerModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
--- src/app/tournaments/tournaments.component.ts
+++ src/app/tournaments/tournaments.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { MatCardModule} from '@angular/material/card';
+import { MatLine } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {MatAccordion, MatExpansionModule} from '@angular/material/expansion';
+
 
 @Component({
   selector: 'app-tournaments',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatListModule,MatIconModule,MatLine,MatAccordion,MatExpansionModule],
   templateUrl: './tournaments.component.html',
   styleUrl: './tournaments.component.css'
 })
